9f78076d4aefa1d4ff07c0a8455f6fc45d22d35a
SuhanAmin <zorofurryking@gmail.com>
2025-07-03 13:31:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ app.post('/submit',upload.single('filename'),async (req, res) => {
     let{title,price}=req.body;
     
     let post=await postmodel.create({
-        filename:req.file.filename,
+        filename:req.file.file.path,
         title,
         price
     });
